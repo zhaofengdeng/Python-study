cff9690c2ff938d4afc25a8d9a937b4e34d6c487
zhaofengdeng <18201075336@139.com>
2019-06-10 22:12:09 +0800

    if与+\学习

---
+++ 02.py
@@ -0,0 +1,13 @@
+#行和缩进
+#学习 Python 与其他语言最大的区别就是，Python 的代码块不使用大括号 {} 来控制类，函数以及其他逻辑判断。python 最具特色的就是用缩进来写模块。
+#缩进的空白数量是可变的，但是所有代码块语句必须包含相同的缩进空白数量，这个必须严格执行。如下所示：
+
+if True:
+    print ("True");
+    print("True1");
+    print("True2");
+else:
+  print ("False");
+  print("False2");
+  print("False3");
+print("new 逻辑判断");
---
+++ 03.py
@@ -0,0 +1,8 @@
+#+\表示加下一行
+total = "test1" + \
+        "test2" + \
+        "test3"
+print(total);
+total="test1" +\
+ "test2"
+print(total);
